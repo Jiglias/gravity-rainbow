19c026092e284185e6ff28207ba5ebd739243905
Jiglias <63088404+Jiglias@users.noreply.github.com>
2024-12-13 19:42:48 +1100

    Update gravity.py
    rainbow gravity viewer

--- gravity.py
+++ gravity.py
@@ -3,8 +3,14 @@ from PIL import Image, ImageDraw
 import math
 
 G = 1
-HEIGHT = 128
-WIDTH = 128
+name = "saw"
+img = Image.open(name+".png")
+
+WIDTH, HEIGHT = img.size
+
+img = img.convert("L")
+
+
 
 def forceGravity(M,m,r):
     global G
@@ -12,6 +18,7 @@ def forceGravity(M,m,r):
 
 
 def calculateEffect(startRow, startCol, grav):
+    
     
     for indexRow, row in enumerate(grav):
 
@@ -27,28 +34,56 @@ def calculateEffect(startRow, startCol, grav):
                 grav[indexRow, indexCol, 1] += force*math.sin(angle)
     return grav
 
+def applyPreset(row, col, grav, intense):
+    intensity = intense / 255
+
+    startX = midX - col # coordinates of were we start on preset
+    startY = midY - row
+    for indexRow in range(HEIGHT):
+        for indexCol in range(WIDTH):
+            currX = startX + indexCol # where we currently are on preset
+            currY = startY + indexRow
+
+            # print([currY, currX, 0],[indexRow, indexCol, 0] )
+            try:
+                grav[indexRow, indexCol, 0] += preset[currY, currX, 0]*intensity
+                grav[indexRow, indexCol, 1] += preset[currY, currX, 1]*intensity
+            except:
+                print(preset[currY, currX],grav[indexRow, indexCol] )
+    return grav
+
+
+
 def convertColour(grav):
-    gravColour = np.zeros(shape=(HEIGHT,WIDTH,3), dtype=np.uint8)
+    gravColour = np.zeros(shape=(HEIGHT,WIDTH,3),  dtype=np.uint8)
     for indexRow, row in enumerate(gravColour):
 
         for indexCol, col in enumerate(row):
             gravX= grav[indexRow, indexCol, 0]
             gravY= grav[indexRow, indexCol, 1]
-            intensity = math.floor(math.sqrt(gravX**2 + gravY**2))
-            gravColour[indexRow, indexCol] = [intensity, 0, 0]
+
+            angle = (math.atan2(gravY,gravX)+math.pi) / (2*math.pi) * 255
+
+            intensity = min(math.floor(math.sqrt(gravX**2 + gravY**2)/3),255)
+
+
+            gravColour[indexRow, indexCol] = [angle, 180, intensity]
     return gravColour
 
 
 
-def createValues():
-    preset = np.zeros(shape=(3*HEIGHT,3*WIDTH,3), dtype=np.uint8)
+midX = 3*WIDTH//2
+midY = 3*HEIGHT//2
+preset = calculateEffect(midY, midX, np.zeros((3*HEIGHT,3*WIDTH, 2)))
+
+grav = np.zeros((HEIGHT,WIDTH, 2))
+
+# img = Image.new("1", (HEIGHT, WIDTH))
+# draw = ImageDraw.Draw(img)
+
+# draw.circle((HEIGHT //2,WIDTH // 2), 16, fill ="white")
 
 
-
-img = Image.new("1", (HEIGHT, WIDTH))
-draw = ImageDraw.Draw(img)
-
-draw.circle((HEIGHT /2,WIDTH / 2), 16, fill ="white")
 
 arr = np.asarray(img)
 
@@ -57,13 +92,15 @@ arr = np.asarray(img)
 for indexRow, row in enumerate(arr):
     print(indexRow)
     for indexCol, col in enumerate(row):
-        if col:
-            grav = calculateEffect(indexCol, indexRow)
+        if col!=0:
+            grav = applyPreset(indexCol, indexRow, grav, col)
 
 img = convertColour(grav)
 print(img)
-img = Image.fromarray(img, "RGB")
+img = Image.fromarray(img, "HSV")
 img.show()
+img = img.convert("RGB")
+img.save(name+"-New.png")
 
 
 
